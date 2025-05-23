cc8fa03d8ec0e672a9445ad534733ac19eb9c8d1
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2025-05-23 07:24:38 +0200

    Install RenderMarkdown

--- init.lua
+++ init.lua
@@ -284,6 +284,19 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
+    'echasnovski/mini.nvim',
+    version = '*',
+  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
