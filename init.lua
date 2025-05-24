32b092d9a1cc1b30680a3ee3dbe4fa3d97eedbd9
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2025-05-24 17:13:02 +0200

    HACK: configure LSPs manually to make them configurable

--- init.lua
+++ init.lua
@@ -697,36 +697,15 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      --
+      --  HACK: We just list the servers here and don't include the full config. Rather than relying
+      --        on Mason handlers, which weren't working, the LSPs are configured one-by-one
+      --        after Mason has been configured.
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
         pyright = {},
         cmake = {},
         fortls = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
-        lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
+        lua_ls = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -751,16 +730,35 @@ require('lazy').setup({
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
         automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
+        --        handlers = {
+        --          function(server_name)
+        --            local server = servers[server_name] or {}
+        --            -- This handles overriding only values explicitly passed
+        --            -- by the server configuration above. Useful when disabling
+        --            -- certain features of an LSP (for example, turning off formatting for ts_ls)
+        --            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+        --            require('lspconfig')[server_name].setup(server)
+        --          end,
+        --        },
+      }
+      require('lspconfig').lua_ls.setup {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = 'Replace',
+            },
+          },
         },
+      }
+      require('lspconfig').pyright.setup {
+        capabilities = capabilities,
+      }
+      require('lspconfig').cmake.setup {
+        capabilities = capabilities,
+      }
+      require('lspconfig').fortls.setup {
+        capabilities = capabilities,
       }
     end,
   },
