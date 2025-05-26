5d501fa8f309b66de227adfae7c58ecf6ab8ef0f
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2025-05-26 15:18:00 +0000

    Treat fypp/hypp files as fortran

--- init.lua
+++ init.lua
@@ -220,6 +220,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- treat fypp/hypp as fortran files
+vim.filetype.add({
+  pattern = {
+    [".*fypp"] = 'fortran',
+    [".*hypp"] = 'fortran',
+  },
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
