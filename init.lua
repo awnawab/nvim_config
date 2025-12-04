cf62f78bb52b028320375d1f7beee5814b658c33
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2025-12-04 22:43:44 +0100

    Don't render markdown by default

--- init.lua
+++ init.lua
@@ -336,7 +336,9 @@ require('lazy').setup({
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = {
+      enabled = false, -- Disable rendering by default
+    },
   },
   {
     'farmergreg/vim-lastplace',
