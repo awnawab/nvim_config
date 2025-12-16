a59adf7be4d472d71d2c2435f40ac583ec5e6daf
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2025-12-16 20:37:45 +0100

    More lazy loaded plugins

--- init.lua
+++ init.lua
@@ -331,7 +331,7 @@ require('lazy').setup({
   },
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    ft = { 'markdown' },
+    cmd = { 'RenderMarkdown' },
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
