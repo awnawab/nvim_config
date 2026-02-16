a5417e62886b9182f6f70130bce80750ad5bb0dc
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2026-02-16 10:39:47 +0100

    Enable indent_line

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -3,7 +3,10 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
+    event = 'BufReadPost',
     main = 'ibl',
-    opts = {},
+    opts = {
+      scope = { enabled = false },
+    },
   },
 }
