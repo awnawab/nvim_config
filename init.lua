a4259bca5d23df66fca76dac4ecdb1a26f21386f
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2026-02-16 10:33:03 +0100

    Lsp: add fortls server

--- init.lua
+++ init.lua
@@ -700,6 +700,19 @@ require('lazy').setup({
             end,
           },
         },
+        fortls = {
+          handlers = {
+            ['textDocument/publishDiagnostics'] = function(err, result, ctx, config)
+              -- Filter out specific diagnostics
+              result.diagnostics = vim.tbl_filter(function(diagnostic)
+                local match = string.match(diagnostic.message, 'not found in project')
+                return not match
+              end, result.diagnostics)
+
+              vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx, config)
+            end,
+          },
+        },
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
