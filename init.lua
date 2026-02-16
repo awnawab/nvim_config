685a9142f83c18f1aa88d18afe44732eecc37f4b
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2026-02-16 09:51:04 +0100

    associate .config to toml

--- init.lua
+++ init.lua
@@ -238,6 +238,13 @@ vim.filetype.add {
   },
 }
 
+-- associate .config to toml
+vim.filetype.add {
+  pattern = {
+    ['.*config'] = 'toml',
+  },
+}
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
