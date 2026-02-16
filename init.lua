3525888bc83f40f9b87e7780e175c2c6a3a7b3c3
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2026-02-16 10:04:16 +0100

    Add markdown render, disabled by default

--- init.lua
+++ init.lua
@@ -355,7 +355,18 @@ require('lazy').setup({
       },
     },
   },
-
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    cmd = { 'RenderMarkdown' },
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      enabled = false, -- Disable rendering by default
+    },
+  },
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
